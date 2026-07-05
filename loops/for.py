574620bd6ff406a80b0c767f8e87d7b811741159
Namithaa30 <namithaakp30@gmail.com>
2026-07-05 17:16:30 +0530

    loops commit

---
+++ loops/for.py
@@ -0,0 +1,11 @@
+name = "python"
+for ch in name:
+    print(ch)
+
+numbers = [10,11,12,45,67,78,35]
+for num in numbers:
+    print(num)
+
+name2= "qwertyuiop"
+for i in range  (len(name2)):
+    print(i,name2[i])
